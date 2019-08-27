914102a2bc25fb78236307f1237390d466ee180e
newcobol <newcobol@gmail.com>
2019-08-27 13:09:42 +0900

    test

--- study20190827.py
+++ study20190827.py
@@ -13,3 +13,22 @@ def f(x):
 Y = map(lambda x: x * x + 4 * x + 5, range(10))
 #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print Y
+
+y = map(lambda x: len(x), ["Hello", "Python", "Programing"])
+print y
+
+y = []
+for x in [1, 2, 3, 34]:
+    if x > 2:
+        y.append(x)
+print y
+
+print
+print
+
+print filter(lambda x: x % 2, [1, 2, 3, 4, 5, 6])
+
+print filter(lambda x: x > 2, [1, 2, 4, 3, 34])
+print filter(lambda x: x > 2, (1, 2, 4, 3, 34))
+print filter(lambda x: x < 'a', 'abcABCdefDEF')
+
