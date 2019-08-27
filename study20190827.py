6ea8ae1678328696e673dab54bf45bb8df3544e0
newcobol <newcobol@gmail.com>
2019-08-27 11:34:41 +0900

    test

--- study20190827.py
+++ study20190827.py
@@ -1,1 +1,15 @@
 print 'Hello Python !!!'
+
+def f(x):
+    return x * x
+
+#X = [1, 2, 3, 4, 5]
+#Y = []
+#for x in X:
+#    y = f(x)
+#    Y.append(y)
+#print Y
+
+Y = map(lambda x: x * x + 4 * x + 5, range(10))
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print Y
