0754293344ac6246df6b033d71cfa1909f41f6bd
newcobol <newcobol@gmail.com>
2019-08-27 13:48:32 +0900

    test

--- study20190827.py
+++ study20190827.py
@@ -32,3 +32,35 @@ print filter(lambda x: x > 2, [1, 2, 4, 3, 34])
 print filter(lambda x: x > 2, (1, 2, 4, 3, 34))
 print filter(lambda x: x < 'a', 'abcABCdefDEF')
 
+print
+
+print reduce(lambda x, y: x + y * y, range(1, 11), 0)
+
+x = 0
+for y in range(1, 11):
+    x = x + y * y
+print x
+
+print reduce(lambda x, y: y + x, 'abcde')
+
+t=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+print reduce(lambda x, y : x + y, filter(lambda x: x % 2, t))
+
+print reduce(lambda x, y : x + y, range(1, 11))
+print filter(lambda x: x % 2, range(1, 11))
+
+g = lambda x, y=10: x + y
+print g(10)
+
+print reduce(lambda x, y: x + y * y, range(1, 11), 0)
+
+g = 10
+h = 5
+
+def f(a):
+    h = a + 10
+    b = h + a + g
+    return b
+
+print f(h)
+print h
